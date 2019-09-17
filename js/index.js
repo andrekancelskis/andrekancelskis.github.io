f90bb19972ec21e70326a0af12e61f6665d11138
Logon Aluno <logonaluno@local.com>
2019-09-17 11:38:39 -0300

    responsivo

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.navbar-toggler');
+const menu = document.querySelector('.collapse.navbar-collapse');
+
+btn.addEventListener('click', () => {
+	menu.classList.toggle('is--active');
+});
